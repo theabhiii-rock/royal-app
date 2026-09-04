1494512237896ff7830c405da817d8c633a2cbe1
Demo User <demo@example.com>
2026-09-04 11:26:54 +0530

    Unblock stuck users with force_update: False and enable native browser launch

--- android/app/src/main/java/com/royal/betking/MainActivity.java
+++ android/app/src/main/java/com/royal/betking/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.os.Message;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -13,12 +17,48 @@ public class MainActivity extends BridgeActivity {
         
         try {
             if (this.getBridge() != null && this.getBridge().getWebView() != null) {
-                this.getBridge().getWebView().setDownloadListener(new DownloadListener() {
+                WebView webView = this.getBridge().getWebView();
+                
+                // 1. Download Listener: Open external browser for any download
+                webView.setDownloadListener(new DownloadListener() {
                     @Override
                     public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW);
-                        intent.setData(Uri.parse(url));
-                        startActivity(intent);
+                        try {
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setData(Uri.parse(url));
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                
+                // 2. Support multiple windows and window.open by launching external browser
+                webView.getSettings().setSupportMultipleWindows(true);
+                webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+                
+                webView.setWebChromeClient(new WebChromeClient() {
+                    @Override
+                    public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+                        WebView newWebView = new WebView(view.getContext());
+                        newWebView.setWebViewClient(new WebViewClient() {
+                            @Override
+                            public boolean shouldOverrideUrlLoading(WebView v, String url) {
+                                try {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    view.getContext().startActivity(intent);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                return true;
+                            }
+                        });
+                        WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+                        transport.setWebView(newWebView);
+                        resultMsg.sendToTarget();
+                        return true;
                     }
                 });
             }
