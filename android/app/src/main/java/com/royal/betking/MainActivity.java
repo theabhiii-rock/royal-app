ffb8116b798eaa48416166733752c6701521b20b
Demo User <demo@example.com>
2026-09-04 11:02:07 +0530

    Add native Android DownloadListener and update downloadAppUpdate to launch external browser

--- android/app/src/main/java/com/royal/betking/MainActivity.java
+++ android/app/src/main/java/com/royal/betking/MainActivity.java
@@ -1,5 +1,28 @@
 package com.royal.betking;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.DownloadListener;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        try {
+            if (this.getBridge() != null && this.getBridge().getWebView() != null) {
+                this.getBridge().getWebView().setDownloadListener(new DownloadListener() {
+                    @Override
+                    public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(url));
+                        startActivity(intent);
+                    }
+                });
+            }
+        } catch (Exception ignored) {
+        }
+    }
+}
